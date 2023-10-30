024124f1d238e4c484e197a5e3d0b5481414c052
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-10-30 07:58:52 +0530

    Strategy Design Pattern - Created a Strategy Interface with an Abstract Method

---
+++ src/Behavioral_Design_Pattern/Strategy/PathCalculatorStrategy.java
@@ -0,0 +1,5 @@
+package Behavioral_Design_Pattern.Strategy;
+
+public interface PathCalculatorStrategy { // Step1: Create a Strategy Interface with an Abstract Method
+    void findPath(String source, String destination);
+}
