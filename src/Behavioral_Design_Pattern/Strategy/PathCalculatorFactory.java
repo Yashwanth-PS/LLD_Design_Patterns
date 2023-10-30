50cc4caa5482bb20a13d3a9c8abf136cdde5739b
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-10-30 08:13:32 +0530

    Strategy Design Pattern - Created a Factory Class to get the Object of the Actual Implementation of a Strategy

---
+++ src/Behavioral_Design_Pattern/Strategy/PathCalculatorFactory.java
@@ -0,0 +1,10 @@
+package Behavioral_Design_Pattern.Strategy;
+public class PathCalculatorFactory { // Step4: Create a Factory Class to get the Object of the Actual Implementation of a Strategy
+    public static PathCalculatorStrategy getPathCalculator(TransportationMode mode){
+        return switch (mode){
+            case CAR -> CarPathCalculatorStrategy.getInstance();
+            case BIKE -> BikePathCalculatorStrategy.getInstance();
+            case WALK -> WalkPathCalculatorStrategy.getInstance();
+        };
+    }
+}
