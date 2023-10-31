ccd064f006b689336a89bf524b6f9aa66808f93e
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-10-31 22:54:37 +0530

    Observer Design Pattern - Created the Publisher

---
+++ src/Behavioral_Design_Pattern/Observer/Amazon.java
@@ -0,0 +1,33 @@
+package Behavioral_Design_Pattern.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Amazon { // Publisher --> where the event actually happens
+    private List<OrderPlacedSubscriber> orderPlacedSubscribers; // Contains list of subscriber objects
+    private static Amazon instance;
+    public Amazon() {
+        this.orderPlacedSubscribers = new ArrayList<>();
+    }
+    public void orderPlaced(){ // Event Method
+        for(OrderPlacedSubscriber orderPlacedSubscriber: orderPlacedSubscribers){
+            orderPlacedSubscriber.orderPlaceEvent();
+        }
+    }
+    public void register(OrderPlacedSubscriber orderPlacedSubscriber){
+        orderPlacedSubscribers.add(orderPlacedSubscriber);
+    }
+    public void deregister(OrderPlacedSubscriber orderPlacedSubscriber){
+        orderPlacedSubscribers.remove(orderPlacedSubscriber);
+    }
+    public static Amazon getInstance() {
+        if (instance == null) {
+            synchronized (Amazon.class) {
+                if (instance == null) {
+                    instance = new Amazon();
+                }
+            }
+        }
+        return instance;
+    }
+}
