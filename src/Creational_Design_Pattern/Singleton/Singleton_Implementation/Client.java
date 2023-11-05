a36782421217208226cf34c4d8d193bc7524f970
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-05 11:35:57 +0530

    Singleton - Added Print Statements in Client Class

--- src/Creational_Design_Pattern/Singleton/Singleton_Implementation/Client.java
+++ src/Creational_Design_Pattern/Singleton/Singleton_Implementation/Client.java
@@ -6,5 +6,11 @@ public class Client {
         Singleton s2 = Singleton.getInstance();
         Singleton s3 = Singleton.getInstance();
         Singleton s4 = Singleton.getInstance();
+
+        // Print the hash code (address) of the object
+        System.out.println("Address of the object s1 : " + System.identityHashCode(s1));
+        System.out.println("Address of the object s2 : " + System.identityHashCode(s2));
+        System.out.println("Address of the object s3 : " + System.identityHashCode(s3));
+        System.out.println("Address of the object s4 : " + System.identityHashCode(s4));
     }
 }
